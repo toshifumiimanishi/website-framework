d52682ccf39e7ef2a68af12a5c5e59cfeca233d5
toshifumiimanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-02-10 22:31:50 +0900

    :sparkles: Add: 入力フォームのパスワードを表示する関数の作成

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 import '../sass/main.scss';
+
+const revealPassword = () => {
+  const triggerElement = document.querySelector('.js-revealPassword');
+
+  triggerElement.addEventListener('click', function() {
+    const target = this.getAttribute('data-target');
+    const targetElement = document.querySelector(target);
+    const type = targetElement.type;
+
+    if (type === 'password') {
+      targetElement.type = 'text';
+    } else {
+      targetElement.type = 'password';
+    }
+  });
+};
+
+revealPassword();
