d432eee91acea61425cb06c7181cbe1d8e8f4d0f
toshifumiimanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2020-02-20 08:06:53 +0900

    :recycle: Refactor: 書式変更

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,31 +1,26 @@
 import '../src/sass/main.scss';
-import { storiesOf } from '@storybook/html';
 
 export default {
-  title: 'Demo',
+  title: 'Components',
 };
-
-export const heading = () => '<h1>Hello World</h1>';
 
 export const button = () => {
-  const btn = document.createElement('button');
-  btn.type = 'button';
-  btn.innerText = 'Hello Button';
-  btn.addEventListener('click', e => console.log(e));
-  return btn;
-};
-
-storiesOf('Components', module)
-  .add('Button', () => `
+  const elements = `
     <div class="p-btn">DEFAULT</div>
     <a class="p-btn -primary" href="">PRIMARY</a>
     <button class="p-btn -secondary" type="button">SECONDARY</button>
     <button class="p-btn" type="button" disabled>DISABLED</button>
     <a class="p-btn -outline" href="">OUTLINE</a>
-  `)
-  .add('formLabel', () => `
+  `;
+  return elements;
+};
+
+export const form = () => {
+  const element = `
     <div class="p-floatlabel">
       <input class="c-textfield p-floatlabel_textfield" type="text">
       <label class="p-floatlabel_placeholder">メールアドレスまたは携帯電話番号</label>
     </div>
-  `)
+  `;
+  return element;
+};
